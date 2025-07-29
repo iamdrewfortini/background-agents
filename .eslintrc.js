74a522551dfc5a67b4e9f4b7ff9db137b24d4eec
iamdrewfortini <drew@dacvisuals.com>
2025-07-28 21:47:39 -0500

    fix: Add test script and development tools (eslint, prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    '*.min.js'
+  ]
+}; 
